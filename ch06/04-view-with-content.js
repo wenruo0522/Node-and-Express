b98b287769336c73cd14bb1a937c4183a49a10c0
wenruo <zjm023@mail.ustc.edu.cn>
2021-08-03 16:21:13 +0800

    add: simple methods for chapter 06

--- ch06/04-view-with-content.js
+++ ch06/04-view-with-content.js
@@ -22,9 +22,26 @@ app.get('/greeting', (req, res) => {
     res.render('greeting', {
         message: 'Hello esteemed programmer!',
         style: req.query.style,
-        userid: req.cookie.userid,
+        userid: req.cookies.userid,
         username: req.session.username
     })
 })
 
+app.get('/set-random-userid', (req, res) => {
+    res.cookie('userid', (Math.random()*10000).toFixed(0))
+    res.redirect('/greeting')
+})
 
+app.get('/set-random-username', (req, res) => {
+    req.session.username = catNames.random()
+    res.redirect('/greeting')
+})
+
+app.get('*', (req, res) => {
+    res.send('Check out our <a href="/greeting">greeting</a> page!')
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`\nnavigate to http://localhost:${port}/greeting\n`)
+})
