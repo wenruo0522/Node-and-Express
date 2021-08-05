c7aba7b0b2ba5a6896ea207690b0e70a848ed85a
wenruo0522 <376567610@qq.com>
2021-08-06 00:26:42 +0800

    add: chapter 07

---
+++ ch07/lib/handlers.js
@@ -0,0 +1,5 @@
+exports.home = (req, res) => res.render('home')
+exports.sectionTest = (req, res) => res.render('section-test')
+exports.notFound = (req, res) => res.render('404')
+
+exports.serverError = (err, req, res, next) => res.render('500')
---
+++ ch07/meadowlark.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+
+const handlers = require('./lib/handlers')
+const weatherMiddleware = require('./lib/middleware/weather')
+const app = express()
+
+// configure Handlebars view engine
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+    helpers: {
+        section: function(name, options) {
+            if (!this._sections) {
+                this._sections = {}
+            }
+            this._sections[name] = options.fn(this)
+            return null
+        }
+    }
+}))
+app.set('view engine', 'handlebars')
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'))
+app.use(weatherMiddleware)
+
+app.get('/', handlers.home)
+app.get('/section-test', handlers.sectionTest)
+
+app.use(handlers.notFound)
+app.use(handlers.serverError)
+
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log( `Express started on http://localhost:${port}; press Ctrl-C to terminate.` )
+    })
+} else {
+    module.exports = app
+}
